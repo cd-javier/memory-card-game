492c164c1f4562b04da561cc8b6bbdf493b5129b
Javier Quiroga <contact@javierquiroga.com>
2025-08-04 21:31:17 +0100

    feat: Set up document

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/base-url',
+  base: '/memory-card-game',
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+  },
 });
